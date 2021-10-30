fb155b3c35127cbf5451c371daad3cc467642dc4
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-10-30 11:46:25 +0530

    project done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonsContainer } from './containers/PokemonsContainer';
+
+export default function App() {
+    const client = new ApolloClient({
+        uri: 'https://graphql-pokemon2.vercel.app/'
+    });
+    
+    return (
+        <ApolloProvider client={client}>
+            <main>
+                <PokemonsContainer />
+            </main>
+        </ApolloProvider>
+    )
+}
---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { Pokemon } from "../components/Pokemon";
+import { GET_POKEMONS } from "../graphql/get-pokemons";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+//import Skeleton from 'react-loading-skeleton';
+
+export function PokemonsContainer() {
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 151 }
+  });
+  const [page, setPage] = React.useState(1);
+  const [docStart, setDocStart] = React.useState(0);
+  //const [loading, setLoading] =  React.useState(true);
+  const handleChange = (event, value) => {
+    setPage(value);
+    setDocStart(docStart + 20);
+  };
+
+  return (
+    <div className="container">
+      <Stack
+        spacing={2}
+        style={{
+          "text-align": "center",
+          margin: "20px auto",
+          padding:"5px",
+          background: "rgba(255,255,255,0.6)",
+        }}
+      >
+        <Typography style={{"font-size":"1.5rem", textTransform: "uppercase"}}>Page: {page}</Typography>
+        <Pagination
+          count={8}
+          page={page}
+          onChange={handleChange}
+          color="secondary"
+        />
+      </Stack>
+      {pokemons &&
+        pokemons
+          .filter(
+            (pokemon) =>
+              pokemon.number >= docStart && pokemon.number <= docStart + 20
+          )
+          .map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+    </div>
+  );
+}
